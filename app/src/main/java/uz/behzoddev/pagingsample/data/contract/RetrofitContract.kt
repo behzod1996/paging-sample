ad0ff5aa669690b269f912d0274678d358665580
Behzod Bozorboev <behzoddev@gmail.com>
2022-02-09 11:33:59 +0900

    Add retrofit contract

---
+++ app/src/main/java/uz/behzoddev/pagingsample/data/contract/RetrofitContract.kt
@@ -0,0 +1,8 @@
+package uz.behzoddev.pagingsample.data.contract
+
+object RetrofitContract {
+    const val ENDPOINT = "r/aww/hot.json"
+    const val LIMIT = "limit"
+    const val AFTER = "after"
+    const val BEFORE = "before"
+}
